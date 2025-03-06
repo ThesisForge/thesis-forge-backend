987d7f7fb78e3da6eccee8f8427dfe349f722bd2
DamyanBG <d.dimitrov.1988@abv.bg>
2025-03-06 09:43:50 +0200

    code format

--- routers/thesis_router.py
+++ routers/thesis_router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 
 from models.thesis_models import Thesis, ThesisCreate
-from queries.thesis_q import insert_thesis, select_theses_by_user_id, select_thesis_by_id
+from queries.thesis_q import (
+    insert_thesis,
+    select_theses_by_user_id,
+    select_thesis_by_id,
+)
 from auth.token import get_current_user_id
 
 
@@ -9,12 +13,14 @@ thesis_router = APIRouter(prefix="/thesis")
 
 
 @thesis_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Thesis)
-async def post_thesis(thesis_data: ThesisCreate, user_id: str = Depends(get_current_user_id)):
+async def post_thesis(
+    thesis_data: ThesisCreate, user_id: str = Depends(get_current_user_id)
+):
     print(thesis_data.user_id)
     print(user_id)
     if thesis_data.user_id != user_id:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE)
-    
+
     thesis = await insert_thesis(thesis_data)
     return thesis
 
@@ -29,9 +35,11 @@ async def get_user_theses(user_id: str = Depends(get_current_user_id)):
 async def get_thesis(thesis_id: str, user_id: str = Depends(get_current_user_id)):
     thesis = await select_thesis_by_id(thesis_id)
     if not thesis:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Thesis not found!")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Thesis not found!"
+        )
+
     if thesis.user_id != user_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    
+
     return thesis
